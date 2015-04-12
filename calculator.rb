54a8f8b416c49ba2cbe4b751b594b75069fca5d8
mehan jayasuriya <mehan.j@gmail.com>
2015-04-12 15:36:22 -0400

    added rapper class

---
+++ calculator.rb
@@ -0,0 +1,11 @@
+require 'pry'
+require 'rapgenius'
+
+
+
+
+
+
+
+puts "Welcome to the Hip-Hop Misogyny Calculator. To get started, please enter the name of a rapper to look up:"
+
---
+++ lib/rapper_class.rb
@@ -0,0 +1,12 @@
+class Rapper
+
+	attr_accessor :name, :pussy_score, :bitch_score, :ho_score
+
+	def initialize(rapper_name, rapper_pussy_score, rapper_bitch_score, rapper_ho_score) 
+		@name = rapper_name
+		@pussy_score = rapper_pussy_score
+		@bitch_score = rapper_bitch_score
+		@ho_score = rapper_ho_score
+	end
+
+end
