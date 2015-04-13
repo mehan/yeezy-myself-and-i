597f9dfa672aa79059778a9e8e768c7781a855f1
mehan jayasuriya <mehan.j@gmail.com>
2015-04-12 22:54:23 -0400

    tweaked some language

--- calculator.rb
+++ calculator.rb
@@ -3,7 +3,7 @@ require 'rapgenius'
 require_relative 'lib/rapper_class'
 
 puts "\n" 
-puts "Welcome to the Hip-Hop Misogyny Calculator. To get started, please enter the name of a rapper to look up:"
+puts "Welcome to the Hip-Hop Misogyny Calculator. To get started, please enter the name of a rapper you would like to look up:"
 
 rapper_name = gets.strip
 
