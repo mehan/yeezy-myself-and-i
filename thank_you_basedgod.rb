aee72c6d4f739d5f9a4871d102a4350f6f89f0f0
mehan <mehan.j@gmail.com>
2015-05-10 00:15:08 -0400

    removed calculator.rb file

--- thank_you_basedgod.rb
+++ thank_you_basedgod.rb
@@ -1,7 +1,3 @@
-require 'rapgenius'
-require 'net/http' 
-require 'tumblr_client'
-
 Tumblr.configure do |config|
   config.consumer_key = ENV["consumer_key"]
   config.consumer_secret = ENV["consumer_secret"]
