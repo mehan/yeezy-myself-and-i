69b5d5f9f554c0b631b2b203d7af978d53f614c5
mehan <mehan.j@gmail.com>
2015-05-10 00:54:49 -0400

    changed 'access' to 'oauth' in ENV variable calls

--- thank_you_basedgod.rb
+++ thank_you_basedgod.rb
@@ -6,8 +6,8 @@ require 'pry'
 Tumblr.configure do |config|
   config.consumer_key = ENV['consumer_key']
   config.consumer_secret = ENV['consumer_secret']
-  config.oauth_token = ENV['access_token']
-  config.oauth_token_secret = ENV['access_token_secret']
+  config.oauth_token = ENV['oauth_token']
+  config.oauth_token_secret = ENV['oauth_token_secret']
 end
 
 http = Net::HTTP.new(@host, @port)
@@ -58,7 +58,5 @@ randomly_based = based_lyrics.shuffle
 poem = randomly_based[1]+'<br>'+randomly_based[2]+'<br>'+randomly_based[3]+'<br>'+randomly_based[4]+'<br>'+randomly_based[5]
 
 client = Tumblr::Client.new
-
-binding.pry
 
 client.text("based-poetry.tumblr.com", {:body => poem}) 
